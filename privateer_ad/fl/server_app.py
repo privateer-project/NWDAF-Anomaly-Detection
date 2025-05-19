2f79035bf9fbbc969e19cb74cdc44e86a36423fa
Giorgos Livanos <glivanos@infili.com>
2025-05-19 12:02:11 +0300

    Refactor FL client and server for modularity and efficiency
    Replaced redundant code with utility functions for setting/getting weights, simplifying logic. Enhanced model saving mechanism with conditional saving of the best model and improved MLflow integration, ensuring better experiment tracking. Updated parameter handling and logging for clearer reporting and more consistent configuration usage.

--- privateer_ad/fl/server_app.py
+++ privateer_ad/fl/server_app.py
@@ -15,21 +15,20 @@ def main(driver: Driver, context: Context) -> None:
     secaggr_cfg = SecureAggregationConfig()
     # todo set configs to context
     dp_cfg = DifferentialPrivacyConfig()
-    n_clients = context.run_config.get('n_clients')
+    n_clients = context.run_config.get('n-clients')
     num_rounds = context.run_config.get('num-server-rounds')
-    run_name = context.run_config.get('run-name')
 
     strategy = CustomStrategy(noise_multiplier=dp_cfg.noise_multiplier,
-                              clipping_norm=dp_cfg.max_grad_norm,
-                              num_sampled_clients=n_clients,
-                              run_name=run_name)
-    if strategy.parent_run_id:
-        mlflow.log_params({'num_clients': n_clients,
-                           'num_rounds': num_rounds},
-                          run_id=strategy.parent_run_id)
+                              clipping_norm=dp_cfg.server_clipping_norm,
+                              num_sampled_clients=n_clients)
+    if strategy.mlflow_config.track:
+        with mlflow.start_run(run_id=strategy.run_id):
+            mlflow.log_params({'num_clients': n_clients,
+                               'num_rounds': num_rounds})
 
     workflow = DefaultWorkflow(fit_workflow=SecAggPlusWorkflow(num_shares=secaggr_cfg.num_shares,
-                                                               reconstruction_threshold=secaggr_cfg.reconstruction_threshold))
+                                                               reconstruction_threshold=secaggr_cfg.reconstruction_threshold,
+                                                               max_weight=200000))
     # Execute workflow
     strategy.logger.info(f'Server will run for {num_rounds} rounds')
     strategy.logger.info(f'SecAgg+ config: num_shares: {secaggr_cfg.num_shares} reconstruction threshold: {secaggr_cfg.reconstruction_threshold}')
--- privateer_ad/fl/strategy.py
+++ privateer_ad/fl/strategy.py
@@ -1,11 +1,11 @@
-from typing import List, Tuple, Union, Optional, OrderedDict
+from typing import List, Tuple, Union, Optional
 
 import numpy as np
 import torch
 import mlflow
 from mlflow.models import infer_signature
 
-from flwr.common import Scalar, Parameters, Metrics, parameters_to_ndarrays, ndarrays_to_parameters, FitIns, FitRes
+from flwr.common import ndarrays_to_parameters, parameters_to_ndarrays, Scalar, Parameters, Metrics, FitIns, FitRes
 from flwr.common.logger import FLOWER_LOGGER
 from flwr.server.strategy import FedAvg, DifferentialPrivacyClientSideFixedClipping
 from flwr.server.client_proxy import ClientProxy
@@ -15,6 +15,7 @@ from privateer_ad.data_utils.transform import DataProcessor
 from privateer_ad.evaluate.evaluator import ModelEvaluator
 from privateer_ad.models import AttentionAutoencoder, AttentionAutoencoderConfig
 from privateer_ad.config import PathsConf, MLFlowConfig, HParams
+from privateer_ad.fl.utils import set_weights
 
 def metrics_aggregation_fn(results: List[Tuple[int, Metrics]]):
     weighted_sums = {}
@@ -34,15 +35,23 @@ def config_fn(server_round: int):
     return {'server_round': server_round}
 
 class CustomStrategy(DifferentialPrivacyClientSideFixedClipping):
-    def __init__(self, noise_multiplier: float, clipping_norm: float, num_sampled_clients: int, run_name:str):
+    def __init__(self, noise_multiplier: float, clipping_norm: float, num_sampled_clients: int):
+        model_config = AttentionAutoencoderConfig()
+        self.model = AttentionAutoencoder(config=model_config)
+        initial_parameters = ndarrays_to_parameters([val.cpu().numpy() for _, val in self.model.state_dict().items()])
+        base_strategy = FedAvg(on_fit_config_fn=config_fn,
+                               on_evaluate_config_fn=config_fn,
+                               fit_metrics_aggregation_fn=metrics_aggregation_fn,
+                               evaluate_metrics_aggregation_fn=metrics_aggregation_fn,
+                               initial_parameters=initial_parameters)
+        super().__init__(base_strategy, noise_multiplier, clipping_norm, num_sampled_clients)
+
         self.paths = PathsConf()
         self.mlflow_config = MLFlowConfig()
         hparams = HParams()
-        model_config = AttentionAutoencoderConfig()
-        self.model = AttentionAutoencoder(config=model_config)
-        data_processor = DataProcessor()
         self.logger = FLOWER_LOGGER
-        self.test_dl = data_processor.get_dataloader('test',
+        self.data_processor = DataProcessor()
+        self.test_dl = self.data_processor.get_dataloader('test',
                                                  use_pca=hparams.use_pca,
                                                  batch_size=hparams.batch_size,
                                                  seq_len=hparams.seq_len,
@@ -52,35 +61,27 @@ class CustomStrategy(DifferentialPrivacyClientSideFixedClipping):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.evaluator = ModelEvaluator(criterion=hparams.loss, device=self.device)
 
-        initial_parameters = ndarrays_to_parameters([val.cpu().numpy() for _, val in self.model.state_dict().items()])
-        base_strategy = FedAvg(on_fit_config_fn=config_fn,
-                               on_evaluate_config_fn=config_fn,
-                               fit_metrics_aggregation_fn=metrics_aggregation_fn,
-                               evaluate_metrics_aggregation_fn=metrics_aggregation_fn,
-                               initial_parameters=initial_parameters)
-        super().__init__(base_strategy, noise_multiplier, clipping_norm, num_sampled_clients)
-
         self.best_loss = np.Inf
-        self.parent_run_id = None
 
         if self.mlflow_config.track:
             mlflow.set_tracking_uri(self.mlflow_config.server_address)
             mlflow.set_experiment(self.mlflow_config.experiment_name)
 
-            # Create a new run
-            parent_runs = mlflow.search_runs(experiment_names=[self.mlflow_config.experiment_name],
-                                             filter_string=f'tags.mlflow.runName = \'{run_name}\'',
+            runs = mlflow.search_runs(experiment_names=[self.mlflow_config.experiment_name],
+                                             filter_string=f'tags.mlflow.runName = \'{self.mlflow_config.server_run_name}\'',
                                              max_results=1)
-            if len(parent_runs) > 0:
+            if len(runs) > 0:
                 # Use existing run
-                self.parent_run_id = parent_runs.iloc[0].run_id
-                self.logger.info(f'Found existing parent run: {run_name} with id: {self.parent_run_id}')
+                self.run_id = runs.iloc[0].run_id
+                self.logger.info(f'Found existing run: {self.mlflow_config.server_run_name} with id: {self.run_id}')
             else:
-                self.logger.info(f'No Parent run with name {run_name} found. Creating parent run.')
-                with mlflow.start_run(run_name=run_name):
-                    self.parent_run_id = mlflow.active_run().info.run_id
+                # Create a new run
+                self.logger.info(f'No run with name {self.mlflow_config.server_run_name} found. Creating parent run.')
+                with mlflow.start_run(run_name=self.mlflow_config.server_run_name):
+                    self.run_id = mlflow.active_run().info.run_id
 
-        self.trial_path = PathsConf().experiments_dir.joinpath('federated_learning')
+        self.trial_path = PathsConf().experiments_dir.joinpath(self.mlflow_config.server_run_name)
+        self.model_path = self.trial_path.joinpath('best_model.pt')
         self.trial_path.mkdir(exist_ok=True)
 
     def aggregate_fit(self,
@@ -91,41 +92,33 @@ class CustomStrategy(DifferentialPrivacyClientSideFixedClipping):
         """Aggregate model weights using weighted average and store checkpoint"""
         aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
 
-        self.best_loss = aggregated_metrics['val_loss']
-        print(f'Saving round {server_round} aggregated_parameters...')
+        is_best = self.best_loss > aggregated_metrics['val_loss']
+        if is_best:
+            # Save if aggregated loss is improved
+            self.best_loss = aggregated_metrics['val_loss']
+            # Save the model to disk
+            print(f'Saving best model of round {server_round}...')
+            set_weights(self.model, parameters_to_ndarrays(aggregated_parameters))
+            torch.save(self.model.state_dict(), self.model_path)
 
-        # Convert `Parameters` to `list[np.ndarray]`
-        aggregated_ndarrays: list[np.ndarray] = parameters_to_ndarrays(aggregated_parameters)
-        # Convert to PyTorch `state_dict`
-        aggregated_params_dict = zip(self.model.state_dict().keys(), aggregated_ndarrays)
-        state_dict = OrderedDict({k: torch.tensor(v) for k, v in aggregated_params_dict})
-        self.model.load_state_dict(state_dict, strict=True)
+        if self.mlflow_config.track:
+            with mlflow.start_run(run_id=self.run_id):
+                mlflow.log_metrics(aggregated_metrics, step=server_round)
+                if is_best:
+                    self.model.to('cpu')
+                    _input = self.sample.to('cpu')
+                    _output = self.model(_input)
+                    if isinstance(_output, dict):
+                        _output = {key: val.detach().numpy() for key, val in _output.items()}
+                    else:
+                        _output = _output.detach().numpy()
 
-        # Save the model to disk
-        model_path = self.trial_path.joinpath('model.pt')
-        torch.save(self.model.state_dict(), model_path)
-        # Log to MLFlow
-        self.model.to('cpu')
-        _input = self.sample.to('cpu')
-        _output = self.model(_input)
-        if isinstance(_output, dict):
-            _output = {key: val.detach().numpy() for key, val in _output.items()}
-        else:
-            _output = _output.detach().numpy()
-
-        if self.parent_run_id:
-            mlflow.pytorch.log_model(pytorch_model=self.model,
-                                     artifact_path=f'fl_model_{server_round}',
-                                     registered_model_name=f'fl_model_{server_round}',
-                                     signature=infer_signature(model_input=_input.detach().numpy(),
-                                                               model_output=_output),
-                                     pip_requirements=self.paths.root.joinpath('requirements.txt').as_posix(),
-                                     run_id=self.parent_run_id)
-
-            mlflow.log_metrics(aggregated_metrics,
-                               step=server_round,
-                               run_id=self.parent_run_id)
-
+                        mlflow.pytorch.log_model(pytorch_model=self.model,
+                                                 artifact_path=f'best_model_{server_round}',
+                                                 registered_model_name=f'best_model_{server_round}',
+                                                 signature=infer_signature(model_input=_input.detach().numpy(),
+                                                                           model_output=_output),
+                                                 pip_requirements=self.paths.root.joinpath('requirements.txt').as_posix())
         return aggregated_parameters, aggregated_metrics
 
     def configure_fit(self, server_round: int, parameters: Parameters, client_manager: ClientManager) -> List[Tuple[ClientProxy, FitIns]]:
@@ -141,16 +134,13 @@ class CustomStrategy(DifferentialPrivacyClientSideFixedClipping):
     def evaluate(self, server_round: int, parameters: Parameters) -> Optional[tuple[float, dict[str, Scalar]]]:
         """Evaluate model parameters using an evaluation function."""
         self.logger.info('Evaluating model on Server')
-        parameters_ndarrays = parameters_to_ndarrays(parameters)
-        params_dict = zip(self.model.state_dict().keys(), parameters_ndarrays)
-        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
-        self.model.load_state_dict(state_dict, strict=True)
+        set_weights(self.model, parameters_to_ndarrays(parameters))
         self.model.to(self.device)
         metrics, figures = self.evaluator.evaluate(self.model, self.test_dl, prefix='eval', step=server_round)
-        if self.parent_run_id:
-            mlflow.log_metrics(metrics, step=server_round, run_id=self.parent_run_id)
+        if self.mlflow_config.track:
+            with mlflow.start_run(run_id=self.run_id):
+                mlflow.log_metrics(metrics, step=server_round)
                 # Log figures in the parent run
-            with mlflow.start_run(run_id=self.parent_run_id):
                 for name, fig in figures.items():
                     mlflow.log_figure(fig, f'server_{name}_round_{server_round}.png')
         loss = metrics.pop('eval_loss')
