b25fe0d260e456e07dfd815640c3c7286adfcfca
Giorgos Livanos <glivanos@infili.com>
2025-05-20 21:49:02 +0300

    Refactor training pipeline for improved readability and MLFlow consistency
    Simplify code by removing redundant logger and `mlflow.start_run` wrappers. Consolidate run naming, logging, model summary export, and optimizer initialization. Improved handling of differential privacy setup and early stopping configurations.

--- privateer_ad/train/train.py
+++ privateer_ad/train/train.py
@@ -27,24 +27,17 @@ class TrainPipeline:
         self.dp_config = DPConfig()
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        if run_name is None:
-            self.run_name = datetime.now().strftime('%Y%m%d-%H%M%S')
-        else:
-            self.run_name = run_name
-
-        self.logger = setup_logger('local-training')
-        self.logger.info(f'Start run with name: {self.run_name}')
-
-        if self.hparams.apply_dp:
-            self.privacy_engine = PrivacyEngine(accountant='rdp', secure_mode=self.dp_config.secure_mode)
-            self.logger.info('Differential Privacy enabled.')
-
         # Setup MLFlow
         if self.mlflow_config.track:
-            self.logger.info('Initialize MLFlow')
+            logger.info('Initialize MLFlow')
+            self.parent_run_id = None
+            self.run_name = run_name
+            if not self.run_name:
+                self.run_name = datetime.now().strftime('%Y%m%d-%H%M%S')
+
             mlflow.set_tracking_uri(self.mlflow_config.server_address)
             mlflow.set_experiment(self.mlflow_config.experiment_name)
-            self.parent_run_id = None
+
             if nested and self.mlflow_config.server_run_name:
                 parent_runs = mlflow.search_runs(experiment_names=[self.mlflow_config.experiment_name],
                                                  filter_string=f'tags.mlflow.runName = \'{self.mlflow_config.server_run_name}\'',
@@ -61,13 +54,16 @@ class TrainPipeline:
                 self.run_id = runs.iloc[0].run_id
             else:
                 self.run_id = None
-            with mlflow.start_run(run_name=self.run_name, run_id=self.run_id, parent_run_id=self.parent_run_id):
-                self.run_id = mlflow.active_run().info.run_id
 
-        self.logger.info(f'Using device: {self.device}')
-        self.logger.info(f'Starting run {self.run_name}')
+            mlflow.start_run(run_id=self.run_id, run_name=self.run_name, parent_run_id=self.parent_run_id)
 
-        self.logger.info('Setup dataloaders.')
+            self.run_id = mlflow.active_run().info.run_id
+            self.run_name= mlflow.active_run().info.run_name
+            logger.info(f'Run with name {self.run_name} started.')
+
+        logger.info(f'Using device: {self.device}')
+
+        logger.info('Setup dataloaders.')
         self.data_processor = DataProcessor()
 
         # Setup trial dir
@@ -109,70 +105,72 @@ class TrainPipeline:
             from opacus.validators import ModuleValidator
             logger.info('Differential Privacy enabled.')
             ModuleValidator.validate(self.model, strict=True)
+            self.privacy_engine = PrivacyEngine(accountant='rdp', secure_mode=self.dp_config.secure_mode)
 
-    def train_model(self, start_epoch=0):
-        self.model.train()
+            self.model = ModuleValidator.fix(self.model)
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams.learning_rate)
 
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams.learning_rate)
-
-        if self.hparams.apply_dp:
-            self.model, optimizer, self.train_dl = self.privacy_engine.make_private_with_epsilon(
+            self.model, self.optimizer, self.train_dl = self.privacy_engine.make_private_with_epsilon(
                 module=self.model,
-                optimizer=optimizer,
+                optimizer=self.optimizer,
                 data_loader=self.train_dl,
                 epochs=self.hparams.epochs,
                 target_epsilon=self.dp_config.target_epsilon,
                 target_delta=self.dp_config.target_delta,
                 max_grad_norm=self.dp_config.max_grad_norm
             )
+        else:
+            logger.info('Differential Privacy disabled.')
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams.learning_rate)
 
-        self.logger.info('Instantiate ModelTrainer...')
-        self.logger.info('Start Training...')
-        model_summary = summary(self.model,
+        if self.hparams.early_stopping:
+            logger.info('Early stopping enabled.')
+
+        model_summary = summary(model=self.model,
                                 input_data=self.sample,
                                 col_names=('input_size', 'output_size', 'num_params', 'params_percent'))
 
-        if self.mlflow_config.track:
-            with mlflow.start_run(run_id=self.run_id, parent_run_id=self.parent_run_id):
-                mlflow.log_params({'device': self.device})
-                mlflow.log_text(str(model_summary), 'model_summary.txt')
-
-        trainer = ModelTrainer(model=self.model,
-                               optimizer=optimizer,
-                               criterion=self.hparams.loss,
-                               device=self.device,
-                               hparams=self.hparams)
-        if self.mlflow_config.track:
-            with mlflow.start_run(run_id=self.run_id, parent_run_id=self.parent_run_id):
-                best_checkpoint = trainer.training(train_dl=self.train_dl, val_dl=self.val_dl, start_epoch=start_epoch)
-        else:
-            best_checkpoint = trainer.training(train_dl=self.train_dl, val_dl=self.val_dl, start_epoch=start_epoch)
-        # Set model to best so far
-        self.model.load_state_dict(deepcopy(best_checkpoint['model_state_dict']))
-
-        self.logger.info('Training Finished.')
-        if self.mlflow_config.track:
-            with mlflow.start_run(run_id=self.run_id, parent_run_id=self.parent_run_id):
-                if self.hparams.apply_dp:
-                    mlflow.log_params(self.dp_config.__dict__)
-                    mlflow.log_metrics({'epsilon': self.privacy_engine.get_epsilon(self.dp_config.target_delta)},
-                                       step=start_epoch)
-
+        if mlflow.active_run():
+            mlflow.log_params({'device': self.device})
+            mlflow.log_text(str(model_summary), 'model_summary.txt')
         # Save locally
         with self.trial_dir.joinpath('model_summary.txt').open('w') as f:
             f.write(str(model_summary))
+
+
+    def train_model(self, start_epoch=0):
+        logger.info('Start Training...')
+        self.model.train()
+
+        trainer = ModelTrainer(model=self.model,
+                               optimizer=self.optimizer,
+                               criterion=self.hparams.loss,
+                               device=self.device,
+                               hparams=self.hparams)
+
+        if mlflow.active_run():
+            mlflow.log_params(self.hparams.__dict__)
+        best_checkpoint = trainer.training(train_dl=self.train_dl, val_dl=self.val_dl, start_epoch=start_epoch)
+
+        # Set model to best so far
+        self.model.load_state_dict(deepcopy(best_checkpoint['model_state_dict']))
+
+        logger.info('Training Finished.')
+        if mlflow.active_run() and self.hparams.apply_dp:
+            mlflow.log_params(self.dp_config.__dict__)
+            mlflow.log_metrics({'epsilon': self.privacy_engine.get_epsilon(self.dp_config.target_delta)},
+                               step=start_epoch)
+
         torch.save(self.model.state_dict(), self.trial_dir.joinpath('model.pt'))
         return best_checkpoint
 
     def evaluate_model(self, step=0):
         evaluator = ModelEvaluator(criterion=self.hparams.loss, device=self.device)
         self.model.eval()
-        if self.mlflow_config.track:
-            with mlflow.start_run(run_id=self.run_id, parent_run_id=self.parent_run_id):
-                metrics, figures = evaluator.evaluate(self.model, self.test_dl, prefix='eval', step=step)
+        metrics, figures = evaluator.evaluate(self.model, self.test_dl, prefix='eval', step=step)
 
         metrics_logs = '\n'.join([f'{key}: {value}' for key, value in metrics.items()])
-        self.logger.info(f'Test metrics:\n{metrics_logs}')
+        logger.info(f'Test metrics:\n{metrics_logs}')
 
         # Save locally
         for name, fig in figures.items():
