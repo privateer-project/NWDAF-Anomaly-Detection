bc84a61a672a38fbc13371fee312ccaf1d7f36ca
Giorgos Livanos <glivanos@infili.com>
2025-05-20 21:48:23 +0300

    Refactor dataset preparation and processing logic
    Streamline dataset preparation with a dedicated `prepare_datasets` method, enhancing clarity and modularity. Improved scalability by refactoring dataset splitting, scaling, and preprocessing. Adjusted path handling and introduced `_read_csv` for better reusability and error management.

--- privateer_ad/etl/transform.py
+++ privateer_ad/etl/transform.py
@@ -1,5 +1,5 @@
 import os
-from pathlib import Path
+from copy import deepcopy
 
 import joblib
 import pandas as pd
@@ -19,6 +19,7 @@ from privateer_ad.etl.utils import check_existing_datasets, get_dataset_path
 
 class DataProcessor:
     """Main class orchestrating data transform and loading."""
+
     def __init__(self):
         self.metadata = MetaData()
         self.paths = PathsConf()
@@ -28,160 +29,103 @@ class DataProcessor:
         self.scaler = None
         self.input_features = self.metadata.get_input_features()
         self.drop_features = self.metadata.get_drop_features()
-        self.devices = self.metadata.devices
         self.partitioner = None
 
-    def split_data(self, df, save_dir, train_size=0.8):
+    def prepare_datasets(self, raw_dataset_path, train_size: float = 0.8) -> None:
+        """Prepare complete datasets from raw data."""
+        check_existing_datasets()
+        path = get_dataset_path(raw_dataset_path)
+        raw_df = self._read_csv(path)
+        logger.info(f'Loaded data from {path}')
+
+        datasets = self.split_data(raw_df, train_size=train_size)
+
+        self.setup_scaler(self.clean_data(datasets['train']))
+
+        os.makedirs(self.paths.processed, exist_ok=True)
+        logger.info('Save datasets...')
+        for k, df in datasets.items():
+            save_path = get_dataset_path(k)
+            processed_df = self.preprocess_data(df)
+            processed_df.to_csv(save_path, index=False)
+            logger.info(f'{k} saved at {save_path}')
+
+    def split_data(self, df, train_size=0.8):
         train_dfs = []
         val_dfs = []
         test_dfs = []
+        for device, device_info in self.metadata.devices.items():
+            device_df = df.loc[df['imeisv'] == device_info.imeisv]
+            logger.debug(f'Before:\nDevice: {device}, attack samples: {len(device_df[device_df["attack"] == '1'])}'
+                        f' benign samples: {len(device_df[device_df["attack"] == '0'])}')
 
-        for device, device_info in self.devices.items():
-            device_df = df.loc[df['imeisv'] == int(device_info.imeisv)]
-            logger.info(f'Before Device: {device}, attack length: {len(device_df[device_df["attack"] == 1])}'
-                        f' benign length: {len(device_df[device_df["attack"] == 0])}')
-            device_df.loc[device_df['attack_number'].isin(device_info.in_attacks), 'attack'] = 1
-            device_df.loc[~device_df['attack_number'].isin(device_info.in_attacks), 'attack'] = 0
-            logger.info(f'After Device: {device}, attack length: {len(device_df[device_df["attack"] == 1])} '
-                        f'benign length: {len(device_df[device_df["attack"] == 0])}')
-            df_train, df_tmp = train_test_split(device_df,
-                                                train_size=train_size,
-                                                stratify=device_df['attack_number'],
-                                                random_state=42)
-            df_val, df_test = train_test_split( df_tmp,
-                                                test_size=0.5,
-                                                stratify=df_tmp['attack_number'],
-                                                random_state=42)
-            train_dfs.append(df_train)
-            val_dfs.append(df_val)
-            test_dfs.append(df_test)
-        df_train = pd.concat(train_dfs)
-        df_val = pd.concat(val_dfs)
-        df_test = pd.concat(test_dfs)
-        for i, df in enumerate([df_train, df_val, df_test]):
-            logger.info(f'Dataset {i} attack length: {len(df[df["attack"] == 1])} '
-                        f'benign length: {len(df[df["attack"] == 0])}')
-
+            device_in_attacks = device_df['attack_number'].isin(device_info.in_attacks)
+            device_df.loc[device_in_attacks, 'attack'] = '1'
+            device_df.loc[~device_in_attacks, 'attack'] = '0'
+            logger.debug(f'After:\nDevice: {device}, attack samples: {len(device_df[device_df["attack"] == '1'])} '
+                        f'benign samples: {len(device_df[device_df["attack"] == '0'])}')
+            device_train_df, df_tmp = train_test_split(device_df,
+                                                       train_size=train_size,
+                                                       stratify=device_df['attack_number'],
+                                                       random_state=42)
+            device_val_df, device_test_df = train_test_split(df_tmp,
+                                                             test_size=0.5,
+                                                             stratify=df_tmp['attack_number'],
+                                                             random_state=42)
+            train_dfs.append(device_train_df)
+            val_dfs.append(device_val_df)
+            test_dfs.append(device_test_df)
+        df_train, df_val, df_test = pd.concat(train_dfs), pd.concat(val_dfs), pd.concat(test_dfs)
         datasets = {'train': df_train, 'val': df_val, 'test': df_test}
-        [logger.info(f'{key} shape: {df.shape}') for key, df in datasets.items()]
-        os.makedirs(save_dir, exist_ok=True)
-        logger.info('Save datasets...')
-        for key, df in datasets.items():
-            df.to_csv(get_dataset_path(key), index=False)
-        logger.info(f'Datasets saved at {save_dir}')
+
+        for k, df in datasets.items():
+            logger.debug(f'Dataset {k} attack length: {len(df[df["attack"] == '1'])}'
+                        f' benign length: {len(df[df["attack"] == '0'])}'
+                        f'{k} shape: {df.shape}')
+        return datasets
 
     def clean_data(self, df):
-        df = df.drop(columns=self.drop_features)
+        if '_time' in df.columns:
+            _l = len(df)
+            df = df[df['_time'] != '<NA>']
+        df = df.drop(columns=self.drop_features, errors='ignore')
         df = df.drop_duplicates()
         df = df.dropna(axis='rows')
         df.reset_index(drop=True, inplace=True)
         return df
 
-    def setup_scaler(self, df):
-        """
-        Scaling and normalizing numeric values, imputing missing values, clipping outliers,
-         and adjusting values that have skewed distributions.
-        """
-        benign_df = df[df['attack_number'] == 0].copy()
-        os.makedirs(os.path.dirname(self.scaler_path), exist_ok=True)
-
-        self.scaler = StandardScaler()
-        self.scaler.fit(benign_df[self.input_features])
-        joblib.dump(self.scaler, self.scaler_path)
-        return self.scaler
-
-    def load_scaler(self):
-        if self.scaler is None:
-            self.scaler =  joblib.load(self.scaler_path)
-        return self.scaler
-
-    def apply_scale(self, df):
-        """Scale numeric features using standardization (mean=0, std=1)."""
-        self.load_scaler()
-        transformed = self.scaler.transform(df[self.input_features])
-        df.loc[:, self.input_features] = transformed
-        return df
-
-    def setup_pca(self, df, n_components=10):
-        """Reduce feature dimensions using PCA."""
-        self.pca = PCA(n_components=n_components)
-        benign_df = df[df['attack_number'] == 0].copy()
-        self.pca.fit(benign_df[self.input_features])
-        os.makedirs(os.path.dirname(self.pca_path), exist_ok=True)
-        joblib.dump(self.pca, self.pca_path)
-        return self.pca
-
-    def load_pca(self):
-        if self.pca is None:
-            self.pca =  joblib.load(self.pca_path)
-        return self.pca
-
-    def apply_pca(self, df):
-        self.load_pca()
-        pca_features = self.pca.transform(df[self.input_features])
-        pca_df = pd.DataFrame(pca_features, columns=self.pca.get_feature_names_out())
-        df = df.drop(columns=self.input_features)
-        df = df.assign(**pca_df.to_dict(orient='list'))
-        return df
-
-    def initialize_data_pipeline(self,
-                                 dataset_path,
-                                 save_dir,
-                                 n_components: int = None,
-                                 train_size: float = 0.8) -> None:
-        """Prepare complete datasets from raw data."""
-        check_existing_datasets()
-        raw_df = pd.read_csv(dataset_path)
-        logger.info(f'Raw shape: {raw_df.shape}')
-        self.split_data(raw_df, save_dir=save_dir, train_size=train_size)
-        self.preprocess_data('train',
-                             use_pca=False,
-                             n_components=n_components,
-                             setup=True)
-
     def preprocess_data(self,
-                        path: str | Path,
-                        use_pca: bool=False,
-                        n_components=None,
+                        df,
                         partition_id=0,
-                        num_partitions=1,
-                        setup=False) -> DataFrame:
-        if setup:
-            if path in ('val', 'test'):
-                logger.error(f'Setup on {path} is not allowed.')
-                raise ValueError(f'Setup on {path} is not allowed.')
-            logger.warning(f'{"#" * 30} Scalers will be fitted on {path}. {"#" * 30}')
-        if path in ('train', 'val', 'test'):
-            path = get_dataset_path(path)
-        try:
-            df = pd.read_csv(path, low_memory=False)
-        except FileNotFoundError:
-            raise FileNotFoundError(f'File {path} not found.')
-        df = self.get_partition(df, partition_id=partition_id, num_partitions=num_partitions)
+                        num_partitions=1) -> DataFrame:
+        if num_partitions < 1:
+            raise ValueError(f'num_partitions ({num_partitions}) < 1')
+        elif num_partitions == 1:
+            logger.warning(f'partition_id ({partition_id}) is ignored when '
+                           f' when num_partitions == 1')
+        else:
+            if num_partitions > 1 <= partition_id:
+                raise ValueError(f'partition_id ({partition_id}) is greater than num_partitions ({num_partitions})')
+            if partition_id < 0:
+                raise ValueError(f'partition_id ({partition_id}) < 0')
+            df = self.get_partition(df, partition_id=partition_id, num_partitions=num_partitions)
         df = self.clean_data(df)
-        if setup:
-            self.setup_scaler(df)
         df = self.apply_scale(df)
-        if setup:
-            self.setup_pca(df, n_components=n_components)
-        if use_pca:
-            df = self.apply_pca(df)
-        df = df.sort_values('_time').reset_index(drop=True)
+        df = df.sort_values(by=['_time']).reset_index(drop=True)
         return df
 
     def get_partition(self, df: DataFrame, partition_id=0, num_partitions=1) -> DataFrame:
         """Partition data based on provided configuration."""
-        logger.info(f'partition_id: {partition_id} - num_partitions: {num_partitions}')
+        logger.info(f'partition: {partition_id}/{num_partitions}')
         #  if name != 'support'
         if num_partitions == 1:
             num_classes_per_partition = 3
         elif num_partitions == 3:
             num_classes_per_partition = 1
         else:
-             num_classes_per_partition = min(3 // num_partitions, 1)
+            num_classes_per_partition = min(3 // num_partitions, 1)
 
         if self.partitioner is None:
-            # _time,imeisv,5g_tmsi,amf_ue_id,bearer_0_apn,bearer_0_dl_total_bytes,bearer_0_ip,bearer_0_ipv6,bearer_0_pdu_session_id,bearer_0_qos_flow_id,bearer_0_sst,bearer_0_ul_total_bytes,bearer_1_apn,bearer_1_dl_total_bytes,bearer_1_ip,bearer_1_pdu_session_id,bearer_1_qos_flow_id,bearer_1_sst,bearer_1_ul_total_bytes,dl_bitrate,ran_id,ran_plmn,ran_ue_id,registered,rnti,t3512,tac,tac_plmn,ue_aggregate_max_bitrate_dl,ue_aggregate_max_bitrate_ul,ul_bitrate,bearer_1_ipv6,cell,ul_retx,ul_err,ul_mcs,ul_n_layer,ul_path_loss,ul_phr,ul_rank,dl_err,dl_mcs,dl_retx,dl_tx,cqi,epre,initial_ta,p_ue,pusch_snr,ri,turbo_decoder_avg,turbo_decoder_max,turbo_decoder_min,ul_tx,cell_id,attack,malicious,attack_number
             self.partitioner = PathologicalPartitioner(
                 num_partitions=num_partitions,
                 num_classes_per_partition=num_classes_per_partition,
@@ -191,9 +135,45 @@ class DataProcessor:
         partitioned_df = self.partitioner.load_partition(partition_id).to_pandas(batched=False)
         return partitioned_df[df.columns]
 
+    def setup_scaler(self, df):
+        """
+        Scaling and normalizing numeric values, imputing missing values, clipping outliers,
+         and adjusting values that have skewed distributions.
+        """
+        benign_df = df[df['attack'] == '0'].copy()
+        os.makedirs(os.path.dirname(self.scaler_path), exist_ok=True)
+
+        self.scaler = StandardScaler()
+        self.scaler.fit(benign_df[self.input_features])
+        joblib.dump(self.scaler, self.scaler_path)
+        return self.scaler
+
+    def load_scaler(self):
+        if self.scaler is None:
+            self.scaler = joblib.load(self.scaler_path)
+        return self.scaler
+
+    def apply_scale(self, df):
+        """Scale numeric features using standardization (mean=0, std=1)."""
+        self.load_scaler()
+        transformed = self.scaler.transform(df[self.input_features])
+        df.loc[:, self.input_features] = transformed
+        return df
+
+    def _read_csv(self, path):
+        path = get_dataset_path(path)
+        dtypes = deepcopy(self.metadata.get_features_dtypes())
+        dtypes.pop('_time', None)
+        try:
+            return pd.read_csv(path,
+                               dtype=self.metadata.get_features_dtypes(),
+                               parse_dates=['_time'])
+        except FileNotFoundError:
+            raise FileNotFoundError(f'File {path} not found.')
+
+
     def get_dataloader(self,
                        path,
-                       use_pca=False,
                        batch_size=1024,
                        seq_len=6,
                        partition_id=0,
@@ -209,22 +189,20 @@ class DataProcessor:
                              'persistent_workers': True}
 
         logger.info(f'Loading {path} dataset...')
+        df = self._read_csv(path)
+        if path not in ['train', 'val', 'test']:
+            df = self.preprocess_data(df=self._read_csv(path),
+                                      partition_id=partition_id,
+                                      num_partitions=num_partitions)
+        df = df.astype({'attack': int})
 
-        df = self.preprocess_data(path,
-                                  use_pca,
-                                  partition_id=partition_id,
-                                  num_partitions=num_partitions,
-                                  setup=False)
         if only_benign:
             if 'attack' in df.columns:
                 df = df[df['attack'] == 0]
             else:
                 logger.warning('Cannot get benign data. No column named `attack` in dataset.')
-        df = df.sort_values(by=['_time'])
-        if use_pca:
-            input_columns = [column for column in df.columns if column.startswith('pca')]
-        else:
-            input_columns = self.input_features
+
+        # Create time index for each device
         df['time_idx'] = df.groupby('imeisv')['_time'].cumcount()
         dataloader = TimeSeriesDataSet(data=df,
                                        time_idx='time_idx',
@@ -232,16 +210,23 @@ class DataProcessor:
                                        group_ids=['imeisv'],
                                        max_encoder_length=seq_len,
                                        max_prediction_length=1,
-                                       time_varying_unknown_reals=input_columns,
+                                       time_varying_unknown_reals=self.input_features,
                                        scalers=None,
                                        target_normalizer=None,
-                                       allow_missing_timesteps=False
+                                       allow_missing_timesteps=False,
+                                       predict_mode=False,
                                        ).to_dataloader(**dataloader_params)
         logger.info(f'Finished loading {path} dataset.')
         return dataloader
 
+
 if __name__ == '__main__':
-    paths = PathsConf()
     dp = DataProcessor()
-    dp.initialize_data_pipeline(dataset_path=paths.raw_dataset,
-                                save_dir=paths.processed)
+    paths = PathsConf()
+    dp.prepare_datasets(raw_dataset_path=paths.raw_dataset)
+    # for i, sample in enumerate(dl):
+    #     print("i[0]['encoder_cont']", sample[0]['encoder_cont'])
+    #     print("i[0]['encoder_cont']", sample[0]['encoder_cont'].shape)
+    #     if i > 2:
+    #         break
+    exit()
