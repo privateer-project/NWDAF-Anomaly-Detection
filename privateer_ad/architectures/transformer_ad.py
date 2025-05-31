c815d19bc21b54e3c223b1a3f0d5dacdb470d9e2
Giorgos Livanos <glivanos@infili.com>
2025-05-31 18:41:29 +0300

    Refactor `TransformerAD` to standardize configuration handling
    Replaced custom `TransformerADConfig` with the common `ModelConfig` for consistency across modules. Simplified initialization logic by centralizing configuration-related attributes. Updated model components (`embed`, `pos_enc`, `compress`, `output`) to dynamically use `ModelConfig` parameters. Improved maintainability and uniformity in model architecture.

--- privateer_ad/architectures/transformer_ad.py
+++ privateer_ad/architectures/transformer_ad.py
@@ -1,60 +1,43 @@
-from dataclasses import dataclass
-
 from torch import nn
 from opacus.layers import DPMultiheadAttention
 
 from privateer_ad.architectures.layers import PositionalEncoding
+from privateer_ad.config import ModelConfig
 
-@dataclass
-class TransformerADConfig:
-    input_size: int = 8
-    seq_len: int = 12
-    num_layers: int = 1
-    hidden_dim: int = 32
-    latent_dim: int = 16
-    num_heads: int = 1
-    dropout: float = 0.2
 
 class TransformerAD(nn.Module):
     """Attention-based model for anomaly detection."""
 
-    def __init__(self, config: TransformerADConfig=None):
+    def __init__(self, config: ModelConfig = None):
         super(TransformerAD, self).__init__()
-        self.config = TransformerADConfig() if not config else config
-        self.input_size = self.config.input_size
-        self.hidden_dim = self.config.hidden_dim
-        self.latent_dim = self.config.latent_dim
-        self.dropout = self.config.dropout
-        self.num_heads = self.config.num_heads
-        self.num_layers = self.config.num_layers
-        self.seq_len = self.config.seq_len
-        self.embed_dim = 2 * self.hidden_dim
+        self.config = config or ModelConfig()
 
-        self.embed = nn.Linear(self.input_size, self.hidden_dim)
-        self.pos_enc = PositionalEncoding(d_model=self.hidden_dim,
-                                          max_seq_length=self.seq_len,
-                                          dropout=self.dropout)
+        self.embed = nn.Linear(self.config.input_size, self.config.embed_dim)
+        self.pos_enc = PositionalEncoding(d_model=self.config.embed_dim,
+                                          max_seq_length=self.config.seq_len,
+                                          dropout=self.config.dropout)
+
         encoder_layer = nn.TransformerEncoderLayer(
-            d_model=self.hidden_dim,
-            nhead=self.num_heads,
-            dim_feedforward=self.latent_dim,
+            d_model=self.config.embed_dim,
+            nhead=self.config.num_heads,
+            dim_feedforward=self.config.latent_dim,
             batch_first=True
         )
         encoder_layer.self_attn = DPMultiheadAttention(
-            self.hidden_dim,
-            num_heads=self.num_heads,
-            dropout=self.dropout,
+            embed_dim=self.config.embed_dim,
+            num_heads=self.config.num_heads,
+            dropout=self.config.dropout,
             batch_first=True)
 
         self.transformer_encoder = nn.TransformerEncoder(
             encoder_layer,
-            num_layers=self.num_layers,
-            norm=nn.LayerNorm(self.hidden_dim)
+            num_layers=self.config.num_layers,
+            norm=nn.LayerNorm(self.config.embed_dim)
         )
 
-        self.compress = nn.Sequential(nn.Linear(self.hidden_dim, self.latent_dim),
+        self.compress = nn.Sequential(nn.Linear(self.config.embed_dim, self.config.latent_dim),
                                       nn.ReLU())
-        self.output = nn.Linear(self.latent_dim, self.input_size)
+        self.output = nn.Linear(self.config.latent_dim, self.config.input_size)
 
     def forward(self, x):
         x = self.embed(x)
@@ -63,4 +46,3 @@ class TransformerAD(nn.Module):
         x = self.compress(x)
         x = self.output(x)
         return x
-
