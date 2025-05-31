d0a0a358516ffc6035d7ec57d5842d4ddf540f03
Giorgos Livanos <glivanos@infili.com>
2025-05-31 22:09:23 +0300

    Refactor `utils.py` to replace `get_paths` with `PathConfig` for consistent configuration handling

--- privateer_ad/etl/utils.py
+++ privateer_ad/etl/utils.py
@@ -1,12 +1,11 @@
 import os
 
-from privateer_ad.config import get_paths
-
+from privateer_ad.config import PathConfig
 
 def get_dataset_path(dataset_path: str) -> str:
     """Get full path for a dataset file."""
     if dataset_path in ['train', 'val', 'test']:
-        return str(get_paths().processed_dir.joinpath(f"{dataset_path}.csv"))
+        return str(PathConfig().processed_dir.joinpath(f"{dataset_path}.csv"))
     return dataset_path
 
 def check_existing_datasets():
@@ -17,4 +16,4 @@ def check_existing_datasets():
 
 def get_scaler_path(scaler_name: str) -> str:
     """Get full path for a scaler file."""
-    return str(get_paths().scalers_dir.joinpath(f'{scaler_name}.scaler'))
+    return str(PathConfig().scalers_dir.joinpath(f'{scaler_name}.scaler'))
