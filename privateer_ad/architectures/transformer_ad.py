1c2f9f7b068e347a0046a2d7402431b18237cb62
Giorgos Livanos <glivanos@infili.com>
2025-06-01 21:30:11 +0300

    Refactor `TransformerAD` to rename `config` to `model_config` for consistency and improve maintainability

--- privateer_ad/architectures/transformer_ad.py
+++ privateer_ad/architectures/transformer_ad.py
@@ -8,36 +8,36 @@ from privateer_ad.config import ModelConfig
 class TransformerAD(nn.Module):
     """Attention-based model for anomaly detection."""
 
-    def __init__(self, config: ModelConfig = None):
+    def __init__(self, model_config: ModelConfig = None):
         super(TransformerAD, self).__init__()
-        self.config = config or ModelConfig()
+        self.model_config = model_config or ModelConfig()
 
-        self.embed = nn.Linear(self.config.input_size, self.config.embed_dim)
-        self.pos_enc = PositionalEncoding(d_model=self.config.embed_dim,
-                                          max_seq_length=self.config.seq_len,
-                                          dropout=self.config.dropout)
+        self.embed = nn.Linear(self.model_config.input_size, self.model_config.embed_dim)
+        self.pos_enc = PositionalEncoding(d_model=self.model_config.embed_dim,
+                                          max_seq_length=self.model_config.seq_len,
+                                          dropout=self.model_config.dropout)
 
         encoder_layer = nn.TransformerEncoderLayer(
-            d_model=self.config.embed_dim,
-            nhead=self.config.num_heads,
-            dim_feedforward=self.config.latent_dim,
+            d_model=self.model_config.embed_dim,
+            nhead=self.model_config.num_heads,
+            dim_feedforward=self.model_config.latent_dim,
             batch_first=True
         )
         encoder_layer.self_attn = DPMultiheadAttention(
-            embed_dim=self.config.embed_dim,
-            num_heads=self.config.num_heads,
-            dropout=self.config.dropout,
+            embed_dim=self.model_config.embed_dim,
+            num_heads=self.model_config.num_heads,
+            dropout=self.model_config.dropout,
             batch_first=True)
 
         self.transformer_encoder = nn.TransformerEncoder(
             encoder_layer,
-            num_layers=self.config.num_layers,
-            norm=nn.LayerNorm(self.config.embed_dim)
+            num_layers=self.model_config.num_layers,
+            norm=nn.LayerNorm(self.model_config.embed_dim)
         )
 
-        self.compress = nn.Sequential(nn.Linear(self.config.embed_dim, self.config.latent_dim),
+        self.compress = nn.Sequential(nn.Linear(self.model_config.embed_dim, self.model_config.latent_dim),
                                       nn.ReLU())
-        self.output = nn.Linear(self.config.latent_dim, self.config.input_size)
+        self.output = nn.Linear(self.model_config.latent_dim, self.model_config.input_size)
 
     def forward(self, x):
         x = self.embed(x)
