4c094f2055bef3fedde1f2bc88577925be803750
Giorgos Livanos <glivanos@infili.com>
2025-06-03 13:29:28 +0300

    Refactor `predict.py` to standardize logging, simplify method inputs, enhance MLFlow model loading, and improve dataloader integration

--- privateer_ad/predict/predict.py
+++ privateer_ad/predict/predict.py
@@ -1,24 +1,24 @@
+import logging
+
 from typing import Tuple, Optional
 
 import fire
 import numpy as np
 import torch
 
-import mlflow
 from torch import nn
 from tqdm import tqdm
 from sklearn.metrics import classification_report
 
-from privateer_ad import logger
-from privateer_ad.config import ModelConfig, TrainingConfig, MLFlowConfig
+from privateer_ad.config import TrainingConfig, MLFlowConfig, DataConfig
 
 from privateer_ad.etl import DataProcessor
+from privateer_ad.utils import load_champion_model
 
-#todo fully integrate with mlflow model loading
+
 def make_predictions(
-        mlflow_model_path: str,
-        data_path: str,
-        threshold: float,
+        model_name: str ='TransformerAD_DP',
+        data_path: str = 'test',
         seq_len: Optional[int] = None,
         device: Optional[str] = None
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
@@ -27,14 +27,9 @@ def make_predictions(
 
     Parameters:
     -----------
-    model_path : str
-        Path to the saved model state dictionary or the experiment directory (eg. '20250317-100421').
+    model_name : str Model name to load from MLFlow (default: 'TransformerAD_DP')
     data_path : str
-        Path to the CSV file containing the data to evaluate or 'train', 'val', 'test' for prepared datasets.
-    threshold : float
-        Precalculated threshold.
-    batch_size : Optional[int]
-        Override batch size for prediction (uses config default if None)
+        Path to the CSV file containing the data to evaluate or 'train', 'val', 'test' for preprocessed datasets.
     seq_len : Optional[int]
         Override sequence length (uses config default if None)
     device : Optional[str]
@@ -50,48 +45,34 @@ def make_predictions(
     """
 
     # Inject configurations
-    model_config = ModelConfig()
+    mlflow_conf = MLFlowConfig()
     training_config = TrainingConfig()
+    data_config = DataConfig()
+    # Use provided parameters or fall back to config
+    data_config.seq_len = seq_len or data_config.seq_len
 
-    logger.info(f'Starting predictions for model: {mlflow_model_path}')
-    logger.info(f'Making predictions on dataset: {data_path}')
 
     # Setup device
     device = device or ('cuda' if torch.cuda.is_available() else 'cpu')
     device = torch.device(device)
-    logger.info(f'Using device: {device}')
+    logging.info(f'Using device: {device}')
 
     # Setup data processing
-    logger.info('Setting up data processing...')
-    dp = DataProcessor()
+    logging.info('Setting up data processing...')
+    dp = DataProcessor(data_config=data_config)
 
-    # Use provided parameters or fall back to config
-    pred_seq_len = seq_len or model_config.seq_len
-
-    # Get dataloader
     try:
-        dl = dp.get_dataloader(data_path, only_benign=False)   # Always include both classes for prediction
-        logger.info(f'Data loader created successfully for {data_path}')
+        dl = dp.get_dataloader(data_path, only_benign=False, train=False)
+        logging.info(f'Data loader created successfully for {data_path}')
     except Exception as e:
-        logger.error(f'Failed to create data loader: {e}')
+        logging.error(f'Failed to create data loader: {e}')
         raise
-
-    # Get sample for model configuration
-    sample_batch = next(iter(dl))
-    sample_input = sample_batch[0]['encoder_cont'][:1]
-    input_size = sample_input.shape[-1]
-
-    logger.info(f'Detected input size: {input_size}')
-    logger.info(f'Using sequence length: {pred_seq_len}')
-
     # Create model
-    logger.info('Creating model...')
-    mlflow_conf = MLFlowConfig()
-    mlflow.set_tracking_uri(mlflow_conf.server_address)
-
-    model = mlflow.pytorch.load_model(mlflow_model_path)
+    logging.info('Loading model...')
+    model, threshold, loss_fn = load_champion_model(mlflow_conf.tracking_uri, model_name=model_name)
     print(model)
-
+    logging.info(f'Starting predictions for model: {model_name}')
+    logging.info(f'Making predictions on dataset: {data_path}')
     # Move model to device and set to evaluation mode
     model = model.to(device)
     model.eval()
@@ -105,7 +86,7 @@ def make_predictions(
     predictions = []
     labels = []
 
-    logger.info('Making predictions...')
+    logging.info('Making predictions...')
     with torch.no_grad():
         for batch in tqdm(dl, desc='Computing reconstruction errors'):
             x = batch[0]['encoder_cont'].to(device)
@@ -142,10 +123,8 @@ def make_predictions(
     balanced_predictions = np.concatenate([ben_predictions[:_len], mal_predictions[:_len]])
 
     # Log classification report
-    logger.info('Classification Report:')
-    logger.info(classification_report(y_true=balanced_labels, y_pred=balanced_predictions))
-
-    logger.info('Predictions completed successfully')
+    logging.info('Classification Report:')
+    logging.info(classification_report(y_true=balanced_labels, y_pred=balanced_predictions))
     return inputs, losses, predictions, labels
 
 def main():
@@ -154,9 +133,7 @@ def main():
 
 
 if __name__ == '__main__':
-    mlflow_model_path = 'mlflow-artifacts:/304908286791224575/177683639fde4f9b8baa6c4b4a8cfffe/artifacts/model'
     try:
-        make_predictions(
-            mlflow_model_path=mlflow_model_path, data_path='test', threshold=0.028)
+        make_predictions(data_path='test')
     except Exception as e:
-        logger.error(f'Prediction failed: {e}')
+        logging.error(f'Prediction failed: {e}')
