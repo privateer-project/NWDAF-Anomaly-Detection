afa2a18442a3c47d8f513d26d87ac119792f9d14
Giorgos Livanos <glivanos@infili.com>
2025-06-03 11:33:05 +0300

    Refactor `client.py`, `server.py`, and related files to standardize logging and configuration handling
    Replaced custom `logger` with `logging` module across client and server files to match existing standards. Updated `ConfigRecord` and MLFlow-related configurations for consistency (e.g., `parent_run_id`, `child_run_id`). Simplified test dataloader setup in `server.py` and refactored `CustomStrategy` in `strategy.py` to utilize consistent metric handling and MLFlow integration. Streamlined redundant logic to improve code clarity and maintainability.

--- privateer_ad/fl/server.py
+++ privateer_ad/fl/server.py
@@ -1,3 +1,5 @@
+import logging
+
 import mlflow
 import torch
 
@@ -5,7 +7,6 @@ from flwr.common import Context
 from flwr.server import Driver, LegacyContext, ServerApp, ServerConfig
 from flwr.server.workflow import SecAggPlusWorkflow, DefaultWorkflow
 
-from privateer_ad import logger
 from privateer_ad.config import FederatedLearningConfig, MLFlowConfig, PathConfig, TrainingConfig
 from privateer_ad.etl import DataProcessor
 from privateer_ad.evaluate import ModelEvaluator
@@ -26,8 +27,8 @@ def main(driver: Driver, context: Context) -> None:
     """
     mlflow_config = MLFlowConfig()
     _, server_run_id = setup_mlflow(experiment_name=mlflow_config.experiment_name,
-                                    server_address=mlflow_config.server_address)
-    mlflow_config.server_run_id = server_run_id
+                                    tracking_uri=mlflow_config.tracking_uri)
+    mlflow_config.parent_run_id = server_run_id
 
     fl_config = FederatedLearningConfig()
     training_config = TrainingConfig()
@@ -36,17 +37,24 @@ def main(driver: Driver, context: Context) -> None:
     fl_config.n_clients = context.run_config.get('n-clients', fl_config.n_clients)
     fl_config.num_rounds = context.run_config.get('num-server-rounds', fl_config.num_rounds)
 
-    logger.info(f'Federated Learning will run for {fl_config.num_rounds} rounds on {fl_config.n_clients} clients')
-    logger.info(f'Secure aggregation: {fl_config.secure_aggregation_enabled}')
-    logger.info(f'Server MLflow run ID: {server_run_id}')
+    logging.info(f'Federated Learning will run for {fl_config.num_rounds} rounds on {fl_config.n_clients} clients')
+    logging.info(f'Secure aggregation: {fl_config.secure_aggregation_enabled}')
+    logging.info(f'Server MLflow run ID: {server_run_id}')
 
     # Log federated learning parameters to parent run
     mlflow.log_params(fl_config.__dict__)
     mlflow.log_params({'session_type': 'federated_learning'})
 
     data_processor = DataProcessor()
-    test_dl = data_processor.get_dataloader('test', only_benign=False)
-
+    dataloader_params = {'train': False,
+                         'batch_size': data_processor.data_config.batch_size,
+                         'num_workers': data_processor.data_config.num_workers,
+                         'pin_memory': data_processor.data_config.pin_memory,
+                         'prefetch_factor': data_processor.data_config.prefetch_factor,
+                         'persistent_workers': data_processor.data_config.persistent_workers}
+    test_ds = data_processor.get_dataset('test', only_benign=False)  # Already preprocessed
+    test_dl = test_ds.to_dataloader(**dataloader_params)
+    sample = next(iter(test_dl))[0]['encoder_cont'][:1].to('cpu')
     strategy = CustomStrategy(training_config=training_config, mlflow_config=mlflow_config)
 
     # Setup and run FL
@@ -68,41 +76,41 @@ def main(driver: Driver, context: Context) -> None:
     else:
         workflow = DefaultWorkflow()
     try:
-        logger.info('Starting federated learning...')
-        logger.info(f'Server Context:\n{server_context}')
+        logging.info('Starting federated learning...')
+        logging.info(f'Server Context:\n{server_context}')
         workflow(driver, server_context)
-        logger.info('Federated learning completed')
+        logging.info('Federated learning completed')
     finally:
-        logger.info("Performing final evaluation on the global test set...")
+        logging.info("Performing final evaluation on the global test set...")
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         evaluator = ModelEvaluator(device=device)
 
         metrics, _ = evaluator.evaluate(strategy.model, test_dl, prefix='global_test', step=fl_config.num_rounds)
-        logger.info(f"Final global evaluation metrics: {metrics}")
+        logging.info(f"Final global evaluation metrics: {metrics}")
 
         log_model(model=strategy.model,
                   model_name='global_TransformerAD',
-                  dataloader=test_dl,
+                  sample=sample,
                   direction=strategy.training_config.direction,
                   target_metric=strategy.training_config.target_metric,
                   current_target_metric=metrics[strategy.training_config.target_metric],
                   experiment_id=mlflow.get_experiment_by_name(mlflow_config.experiment_name).experiment_id,
                   pip_requirements=PathConfig().requirements_file.as_posix())
 
-        logger.info("Final model logged to MLFlow successfully")
+        logging.info("Final model logged to MLFlow successfully")
         # End parent run
         if mlflow.active_run():
             mlflow.end_run()
 
 
-def setup_mlflow(experiment_name, server_address):
+def setup_mlflow(experiment_name, tracking_uri):
     """Setup MLFlow tracking if enabled."""
     try:
-        mlflow.set_tracking_uri(server_address)
+        mlflow.set_tracking_uri(tracking_uri)
         mlflow.set_experiment(experiment_name)
         if mlflow.active_run():
-            logger.info(
+            logging.info(
                 f"Found active run: {mlflow.active_run().info.run_name} - "
                 f"{mlflow.active_run().info.run_id}, ending it"
             )
@@ -111,8 +119,8 @@ def setup_mlflow(experiment_name, server_address):
         mlflow.start_run()
         run_name = mlflow.active_run().info.run_name
         run_id = mlflow.active_run().info.run_id
-        logger.info(f"MLFlow run started: name: {run_name} (ID: {run_id})")
+        logging.info(f"MLFlow run started: name: {run_name} (ID: {run_id})")
         return run_name, run_id
     except Exception as e:
-        logger.error(f"Failed to setup MLFlow: {e}")
+        logging.error(f"Failed to setup MLFlow: {e}")
         return None, None
